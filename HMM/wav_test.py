488b5409a510aa59f8e991f11906083d728c80d9
robb <Robbluo@163.com>
2019-06-01 12:06:41 +0800

    添加音频测试脚本

--- HMM/wav_test.py
+++ HMM/wav_test.py
@@ -1,14 +1,15 @@
 import sys
 sys.path.append("..")
-from myModule.hmm_optical_sensing import *
-from myModule import common
+from hmm_optical_sensing import *
+import common
 import numpy as np
+import json
 
 
-def load_audio(path):
-    seg = 4 # 2s分段
-    nw = 1024 # 帧长约23ms*4
-    n_mfcc = 32  # mfcc 维数
+def load_audio(path, seg, nw, n_mfcc):
+    # seg = 4 # 2s分段
+    # nw = 1024 # 帧长约23ms*4
+    # n_mfcc = 32  # mfcc 维数
 
     folder_list = common.list_files(path)
     data_dict = {}
@@ -38,7 +39,7 @@ def load_audio(path):
     return data_dict
 
 
-def main(arg):
+def main():
     """
     :param arg:
      输入文件路径：
@@ -54,35 +55,53 @@ def main(arg):
             
     :return: 
     """
-    data_dict = load_audio(arg)
+    with open('.\\test_config.json', encoding='UTF-8') as file:
+        test_config = json.load(file, strict=False)
+    path = test_config['test_data_path']
+    seg_param = test_config['seg_param']
+    seg = seg_param['seg']  # 4s分段
+    nw = seg_param['nw']   # 帧长约23ms*4
+    n_mfcc = seg_param['n_mfcc']   # mfcc 维数
+    save_file = test_config['hmm_model']
+    data_dict = load_audio(path, seg, nw, n_mfcc)
 
-    save_file = '..\\hmms_model_4s_1024.npy'
+    # save_file = '..\\model'
     hmms_model = hmms()
-    hmms_model.load_model(save_file)
+    model_list = common.find_ext_files(save_file, ext='.npy')
+    for model_file in model_list:
+        hmms_model.load_one_model(model_file)  # 加载模型
+    # hmms_model.load_model(save_file)
+    model_num = len(hmms_model.hmms)
 
     for key in data_dict:
-        print('当前预测音频种类：%s' % key)
+        print('当前预测音频文件夹：%s' % key)
         instance = data_dict[key]
         audio_num = instance.get_num()
+        species_count = np.zeros(model_num)
         for j in range(audio_num):
-            print('\t音频名：%s' % instance.audio_name[j])
+            # print('\t音频名：%s' % instance.audio_name[j])
             frame_data = instance.frame[j]
             length = instance.frame_num[j]
             predicts = hmms_model.batch_predict(frame_data, length=length)
             count = np.bincount(predicts)
-            for i in range(len(count)):
-                print('\t\t%s:%d' % (hmms_model.model_name[i], count[i]), end='\t')
+            # for i in range(len(count)):
+            #     print('\t\t%s:%d' % (hmms_model.model_name[i], count[i]), end='\t')
 
             major = np.argmax(count)
             if hmms_model.model_name[major] == '背景音':
                 count[major] = 0
             major = np.argmax(count)
-            print('\n\t\t预测结果：%s\n' % hmms_model.model_name[major])
+            species_count[major] += 1
+            # print('\n\t\t预测结果：%s\n' % hmms_model.model_name[major])
+        print('当前种类识别分布：')
+        species_count /= audio_num
+        for i in range(model_num):
+            print('\t%s: %f' % (hmms_model.model_name[i], species_count[i]))
 
 
 if __name__ == '__main__':
     # main('G:\\Project\\光纤传感\\2018.10.9音频样本\\线路1')
     # main('G:\\Project\\光纤传感\\2018.10.9音频样本\\线路2')
     # main('G:\\Project\\光纤传感\\音频集合')
-    main('E:\DailyResearch\Project\Yang\gmm_hmm\剪辑素材')
+    main()
 
